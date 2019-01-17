e8603edb85841a796912d46a46db254029ce0c9b
BalazsArva <balazs.arva@windowslive.com>
2019-01-17 14:57:20 +0100

    Created sandbox project for experiments.

---
+++ src/BalazsArva.RavenDb.Extensions.ConditionalPatch.Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BalazsArva.RavenDb.Extensions.ConditionalPatch.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
