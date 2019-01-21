1b7b3c2f813daeac1f0b5cf6b48ca25d859134db
BalazsArva <balazs.arva@windowslive.com>
2019-01-21 13:01:30 +0100

    Refactorings.

---
+++ src/BalazsArva.RavenDb.Extensions.ConditionalPatch.SanityTests/UnaryOperations/ArrayLengthTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using BalazsArva.RavenDb.Extensions.ConditionalPatch.Factories;
+using BalazsArva.RavenDb.Extensions.ConditionalPatch.SanityTests.TestDocuments;
+using BalazsArva.RavenDb.Extensions.ConditionalPatch.Utilitites;
+using NUnit.Framework;
+
+namespace BalazsArva.RavenDb.Extensions.ConditionalPatch.SanityTests.UnaryOperations
+{
+    [TestFixture]
+    public class ArrayLengthTests
+    {
+        [Test]
+        public void TryProcess_ArrayLengthOfParameter_ReturnsTrueAndCreatesCorrectScript()
+        {
+            var result = GetParsedJavaScript(doc => doc.SomeIntArray.Length != 0);
+
+            Assert.AreEqual("(this.SomeIntArray.length != args.__param1)", result.script);
+            Assert.AreEqual(1, result.parameters.Count);
+            Assert.AreEqual(0, result.parameters["__param1"]);
+        }
+
+        private (string script, ScriptParameterDictionary parameters) GetParsedJavaScript<TProperty>(Expression<Func<TestDocument, TProperty>> expression)
+        {
+            var processor = ExpressionProcessorPipelineFactory.CreateExpressionProcessorPipeline();
+
+            var parameters = new ScriptParameterDictionary();
+            var script = processor.ProcessExpression(expression, parameters);
+
+            return (script, parameters);
+        }
+    }
+}
